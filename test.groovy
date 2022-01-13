c686fc91296b996caf7e4e8167b338ad47219361
Charles <chsaeydev@gmail.com>
2022-01-13 10:34:26 -0800

    test

--- test.groovy
+++ test.groovy
@@ -1,5 +1,1 @@
-withCredentials([string(credentialsId: 'amazon', variable: 'VAR')]) {
-  // available as an env variable, but will be masked if you try to print it out any which way
-  // note: single quotes prevent Groovy interpolation; expansion is by Bourne Shell, which is what you want
-  sh 'echo $VAR'
-}
+Helloawagsdgl
